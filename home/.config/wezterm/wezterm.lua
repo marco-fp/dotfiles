aa0e4a4f8d95b1ed2a927634aa5d4500ed189201
Marco Fernandez Pranno <mfernandezpranno@gmail.com>
2026-07-07 10:15:06 +0200

    Nord theme for herdr; iTerm2-style cmd+k clear in wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -8,4 +8,16 @@ config.font_size = 13.0
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+config.keys = {
+	-- cmd+k clears like iTerm2: wipe scrollback + screen, then redraw the prompt
+	{
+		key = "k",
+		mods = "CMD",
+		action = wezterm.action.Multiple({
+			wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+			wezterm.action.SendKey({ key = "l", mods = "CTRL" }),
+		}),
+	},
+}
+
 return config
